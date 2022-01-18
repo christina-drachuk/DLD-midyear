358d7f3e65598a702684bff10c3e298a4a057ff9
Nathaniel Lord <44natel@gmail.com>
2022-01-18 10:29:15 -0500

    MyJava

---
+++ MyJavaFx.java
@@ -0,0 +1,21 @@
+package chap14_learning;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MyJavaFx extends Application {
+
+    public void start(Stage primaryStage) {
+        Button btOk = new Button("OK");
+        Scene scene = new Scene(btOk, 200, 250);
+        primaryStage.setTitle("MyJavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
