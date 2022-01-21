8f445ce986c1132c65ce85548e966412952d3cf9
CHRISTINA DRACHUK <cdrachuk@gmail.com>
2022-01-21 12:19:14 -0500

    tried label and failed :/

--- CDAlertBox.java
+++ CDAlertBox.java
@@ -1,14 +1,16 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+// import javafx.event.ActionEvent;
+// import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class CDAlertBox extends Application{
 
     Button button;
+    Label label;
     public static void main(String[] args) {
         launch(args); // call launch, go into app, and set up program as javafx app    
 
@@ -17,8 +19,7 @@ public class CDAlertBox extends Application{
     @Override
     public void start(Stage arg0) throws Exception {
         arg0.setTitle("Title of the window");
-        button = new Button("button");
-        // button.setText("Click me");, this works too
+        button = new Button("Click here!");
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
