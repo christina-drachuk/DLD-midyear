5b024583d0f5a9a875025fe209527def98cc3c0e
Nathaniel Lord <44natel@gmail.com>
2022-01-26 16:59:24 -0500

    Made the 4 doors seperate

--- MainGUI.java
+++ MainGUI.java
@@ -2,10 +2,12 @@ import javafx.application.Application;
 // import javafx.event.ActionEvent;
 // import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 // import javafx.scene.control.Button;
 // import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.*;
 
 
 public class MainGUI extends Application {
@@ -16,9 +18,28 @@ public class MainGUI extends Application {
     public void start(Stage arg0) throws Exception {
         arg0.setTitle("This is the title of the window we can change it later");
         StackPane layout = new StackPane();
-        // layout.getChildren().add(whatever widget we need);
 
-        Scene scene = new Scene(layout, 400, 300);
+        Button door1 = new Button("Click here! 1");
+        door1.setPrefSize(250, 400);
+        door1.setTranslateX(-450);
+        layout.getChildren().add(door1);
+
+        Button door2 = new Button("Click here! 2");
+        door2.setPrefSize(250, 400);
+        door2.setTranslateX(-150);
+        layout.getChildren().add(door2);
+
+        Button door3 = new Button("Click here! 3");
+        door3.setPrefSize(250, 400);
+        door3.setTranslateX(150);
+        layout.getChildren().add(door3);
+
+        Button door4 = new Button("Click here! 4");
+        door4.setPrefSize(250, 400);
+        door4.setTranslateX(450);
+        layout.getChildren().add(door4);
+
+        Scene scene = new Scene(layout, 1250, 700);
         arg0.setScene(scene);
         arg0.show();
     }
