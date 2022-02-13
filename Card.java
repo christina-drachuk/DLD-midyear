97a6e9d27b812746ca66377a36d3167c2de62596
Nathaniel Lord <44natel@gmail.com>
2022-02-13 01:03:07 -0500

    wrote a TON of blackjack

---
+++ Card.java
@@ -0,0 +1,27 @@
+public class Card {
+    int rank;
+    int suit;
+
+    public Card(int rank, int suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+   public static void main(String[] args) {
+    
+   }
+public int getRank() {
+    return rank;
+}
+public void setRank(int rank) {
+    this.rank = rank;
+}
+public int getSuit() {
+    return suit;
+}
+public void setSuit(int suit) {
+    this.suit = suit;
+}
+
+   
+
+}
